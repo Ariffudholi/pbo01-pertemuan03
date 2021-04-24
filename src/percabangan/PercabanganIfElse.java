6ca8d061459fb2e5d996adb02eddbfacc815cc31
Ariffudholi <reginamudawamah@gmail.com>
2021-04-24 18:44:49 +0800

    percabangan if else

---
+++ src/percabangan/PercabanganIfElse.java
@@ -0,0 +1,17 @@
+package percabangan;
+
+public class PercabanganIfElse {
+    public static void main(String[] args); {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja) {
+            double angsul = uangDiDompet - totalBelanja;
+            System.out.println("uang cukup, angsul: " + angsul);
+        }else{
+            System.out.println("uang pas.. Uwuuuuu...");
+        }
+    }
+}
